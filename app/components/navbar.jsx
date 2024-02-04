b5c64521ff3603208d4cf0b50cd42bf0a405aea7
JCzyszczon <jakub.czyszczon1@gmail.com>
2024-02-04 20:56:01 +0100

    update

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -32,18 +32,18 @@ export default function Navbar() {
         <>
         <header className={`sm:w-[90px] w-full sm:h-full overflow-y-scroll no-scrollbar h-auto bg-[#eaecef] dark:bg-[#303134] z-50 rounded-2xl flex sm:flex-col flex-row justify-between items-center sm:py-10 py-3 px-3 ${currentPath ? '' : 'animate-pulse'}`}>
             <div className='sm:w-full w-auto h-auto flex flex-col justify-center items-center gap-1'>
-                <a href='/' className='text-[#0095ff] sm:text-3xl text-2xl font-[700] font-Kalam hover:animate-bounce2 sm:py-5 py-0'>W.</a>
+                <a href='/' aria-label="Go to main page" className='text-[#0095ff] sm:text-3xl text-2xl font-[700] font-Kalam hover:animate-bounce2 sm:py-5 py-0'>W.</a>
             </div>
             <nav className='w-full flex sm:flex-col flex-row sm:justify-center justify-end items-center sm:gap-10 gap-8'>
-                <Link href={'/'}><div onClick={() => setCurrentPath('/')} className={`w-full h-auto flex flex-col justify-center duration-200 items-center ${currentPath === '/' || currentPath.startsWith('/search') ? 'font-[900] hover:text-[##202b3b] text-[##202b3b] gap-2' : 'font-normal hover:text-[#0095ff] text-[#9399a2] gap-1'}`}>
+                <Link href={'/'} aria-label="Go to main page"><div onClick={() => setCurrentPath('/')} className={`w-full h-auto flex flex-col justify-center duration-200 items-center ${currentPath === '/' || currentPath.startsWith('/search') ? 'font-[900] hover:text-[##202b3b] text-[##202b3b] gap-2' : 'font-normal hover:text-[#0095ff] text-[#9399a2] gap-1'}`}>
                     <BsFillCloudSunFill className='sm:text-3xl text-2xl'/>
                     <p className='text-sm sm:flex hidden'>Weather</p>
                 </div></Link>
-                <Link href={'/favourite'}><div onClick={() => setCurrentPath('/favourite')} className={`w-full h-auto flex flex-col justify-center duration-200 items-center gap-1 ${currentPath === '/favourite' ? 'font-[900] hover:text-[##202b3b] text-[##202b3b] gap-2' : 'font-normal hover:text-[#0095ff] text-[#9399a2] gap-1'}`}>
+                <Link href={'/favourite'} aria-label="Go to cities page"><div onClick={() => setCurrentPath('/favourite')} className={`w-full h-auto flex flex-col justify-center duration-200 items-center gap-1 ${currentPath === '/favourite' ? 'font-[900] hover:text-[##202b3b] text-[##202b3b] gap-2' : 'font-normal hover:text-[#0095ff] text-[#9399a2] gap-1'}`}>
                     <BsHeartFill className='sm:text-2xl text-xl'/>
                     <p className='text-sm sm:flex hidden'>Cities</p>
                 </div></Link>
-                <Link href={'/settings'}><div onClick={() => setCurrentPath('/settings')} className={`w-full h-auto flex flex-col justify-center duration-200 items-center gap-1 ${currentPath === '/settings' ? 'font-[900] hover:text-[##202b3b] text-[##202b3b] gap-2' : 'font-normal hover:text-[#0095ff] text-[#9399a2] gap-1'}`}>
+                <Link href={'/settings'} aria-label="Go to settings page"><div onClick={() => setCurrentPath('/settings')} className={`w-full h-auto flex flex-col justify-center duration-200 items-center gap-1 ${currentPath === '/settings' ? 'font-[900] hover:text-[##202b3b] text-[##202b3b] gap-2' : 'font-normal hover:text-[#0095ff] text-[#9399a2] gap-1'}`}>
                     <VscSettings className='sm:text-2xl text-xl'/>
                     <p className='text-sm sm:flex hidden'>Settings</p>
                 </div></Link>
--- app/components/searchBar.jsx
+++ app/components/searchBar.jsx
@@ -16,7 +16,7 @@ export default function SearchBar() {
         <>
         <form onSubmit={formSubmitHandler} className="md:w-3/5 w-full bg-[#eaecef] dark:bg-[#303134] rounded-2xl sm:overflow-hidden overflow-visible">
             <section className="relative">
-                <button type="submit" className="absolute top-0 bottom-0 my-auto text-gray-400 left-3">
+                <button title="Search" aria-label="Search" type="submit" className="absolute top-0 bottom-0 my-auto text-gray-400 left-3">
                     <BiSearchAlt className='w-6 h-6 hover:text-[#0095ff] duration-300'/>
                 </button>
                 <input type="search" name='search' placeholder="Search City" className="w-full py-3 pl-12 rounded-2xl pr-4 duration-300 text-gray-500 outline-none bg-[#eaecef] dark:bg-[#303134] borderColor" onChange={(event) => {setUserInput(event.target.value)}}/>
